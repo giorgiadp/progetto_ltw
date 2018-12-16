53ee0eac8ab09e6dda4d57b1ff93399d49555e3e
Giulai <giu.delci@gmail.com>
2018-12-16 19:49:42 +0100

    polpetta

--- homeStorage.js
+++ homeStorage.js
@@ -1,10 +1,8 @@
 function funzioneCerca(){
     
     var storageIngredienti=JSON.parse(localStorage.getItem('Ricette'));
-
     var stringaIngredienti=document.getElementById("ingredienti").value;
-
-    var listaIngredienti= stringaIngredienti.split(',');
+    var listaIngredienti= stringaIngredienti.replace(/ /g, '').split(',');
 
     //conto quanti degli ingredienti che sto cercando stanno in ogni ricetta
     //quindi mi serve una lista con numero ricetta j e count
@@ -14,7 +12,7 @@ function funzioneCerca(){
     var list=[];
     for(j=0; j<storageIngredienti.length; j++){
         //faccio divetare la stringa array
-        var arrayStorage=storageIngredienti[j].ingredienti.split(',');
+        var arrayStorage=storageIngredienti[j].ingredienti.replace(/ /g, '').split(',');
         //ciclo su ingredienti di ricetta j 
         c=0;
         for(k=0; k<arrayStorage.length; k++){
@@ -30,12 +28,13 @@ function funzioneCerca(){
     }
     
     //trovo la ricetta con il massimo degli ingredienti in comune
-    max=0;
-    ricMax=0
+    var max=0;
+    var ricMax=0
     for (i=0; i<list.length; i++){
-        if(list[i][1] > max)
+        if(list[i][1] > max){
             max=list[i][1];
             ricMax=list[i][0];
+        }
     }
 
     if (max==0){
@@ -43,24 +42,25 @@ function funzioneCerca(){
     }
     else{
         alert("ricetta trovata  " + storageIngredienti[ricMax].titolo);
-        mostraRicetta(storageIngredienti[ricMax]);
+        sessionStorage.setItem('indiceRicetta', JSON.stringify( ricMax));
+        window.location.href="./ricette.html";
         
     }
 
 }
 
-function mostraRicetta(storageRicetta){
-    alert("in mostraRicetta");
-    titolo=storageRicetta.titolo;
-    tempo=storageRicetta.tempo;
-    difficolta=storageRicetta.difficolta;
-    arrayIngredienti=storageRicetta.ingredienti;
-    procedimento=storageRicetta.procedimento;
-    foto=storageRicetta.procedimento;
-    
-    alert(titolo + tempo+ difficolta+ arrayIngredienti+ procedimento+ foto);
-    //devo capire come fare a caricare elemnti su pagina diversa da quella di partenza
-    window.location.href="./ricette.html";
+function mostraRicetta(){
+    cambiabottoniRicetta();
+    var index=JSON.parse(sessionStorage.getItem('indiceRicetta'));
+    var stoRic= JSON.parse(localStorage.getItem('Ricette'))[index];
+
+    document.getElementById('titolo').value= stoRic.titolo;
+    document.getElementById('tempo').value= stoRic.tempo;
+    document.getElementById('difficolta').value= stoRic.difficolta;
+    document.getElementById('ingredienti').value= stoRic.ingredienti;
+    document.getElementById('procedimento').value= stoRic.procedimento;
+    //document.getElementById('foto').value= stoRic.foto;
+   
 }
 
 function funzioneIscriviti(){
@@ -69,6 +69,20 @@ function funzioneIscriviti(){
 
 function funzioneAccedi(){
     window.location.href="./accedi.html";
+}
+
+function cambiabottoniRicetta(){
+    var s=JSON.parse(sessionStorage.getItem('Loggato'));
+    if(s == 'si'){
+        document.getElementById("pulsanti").style.display="none"
+        document.getElementById("pulsanteLogout").style.display="block"
+
+    }
+    else {
+        document.getElementById("pulsanti").style.display="block"
+        document.getElementById("pulsanteLogout").style.display="none"
+    }
+        
 }
 
 function cambiabottoni(){
